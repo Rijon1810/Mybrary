0edb30822e33ed6778ec97c992a942cffca2f7a5
Rijon <rijon.r@yahoo.com>
2020-11-14 16:42:36 +0600

    File Upload Database

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-const fs = require('fs');
 const Book = require("../models/book");
-const uploadPath = path.join('public',Book.coverImageBasePath);
 const Author = require("../models/author");
-const imageMimeTypes = ['image/jpeg','image/png','images/gif']
-const upload = multer ({
-  dest : uploadPath,
-  fileFilter : (req,file,callback) =>{
-    callback(null,imageMimeTypes.includes(file.mimetype))
-  }
-})
-
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
 //All Book Route
 router.get("/", async (req, res) => {
@@ -25,7 +14,7 @@ router.get("/", async (req, res) => {
      query = query.lte('publishDate',req.query.publishedBefore);
   }
   if(req.query.publishedAfter!= null && req.query.publishedAfter !=' ' ){
-     query = query.lte('publishDate',req.query.publishedAfter);
+     query = query.gte('publishDate',req.query.publishedAfter);
   }
   try{
     const books = await query.exec();
@@ -47,7 +36,7 @@ router.get("/new", async (req, res) => {
 });
 
 //Create Book Route
-router.post("/", upload.single('cover'), async (req, res) => {
+router.post("/", async (req, res) => {
    const fileName =req.file != null ? req.file.filename:null;
     
              const book = new Book({
@@ -55,11 +44,10 @@ router.post("/", upload.single('cover'), async (req, res) => {
                author : req.body.author,
                publishDate : new Date(req.body.publishDate),
                pageCount : req.body.pageCount,
-               coverImageName : fileName,
                description : req.body.description,
               // Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.
-              
              })
+             saveCover(book, req.body.cover);
              try{
               const newBook = await book.save();
               //res.redirect(`authors/${newBook.id`);
@@ -67,20 +55,11 @@ router.post("/", upload.single('cover'), async (req, res) => {
               
        }
        catch{
-         if(book.coverImageName !=null)
-         {
-          removeBookCover(book.coverImageName);
-         }
+       
          
         renderNewPage(res, book , true);
        }
 });
-function removeBookCover(fileName){
-  fs,fs.unlink(path.join(uploadPath,fileName),err=>{
-    if(err) console.error(err);
-  })
-
-}
 
 async function renderNewPage (res,book,hasError =false){
   try{
@@ -95,6 +74,15 @@ async function renderNewPage (res,book,hasError =false){
      }
      catch{
        res.redirect('/books');
+     }
+}
+
+function saveCover(book,coverEncoded){
+     if(coverEncoded == null)  return
+     const cover = JSON.parse(coverEncoded);
+     if(cover !=null  && imageMimeTypes.includes(cover.type) ){
+       book.coverImage = new Buffer.from( cover.data,'base64');
+       book.coverImageType = cover.type;
      }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Book = require ('../models/book');
 
 router.get('/', async (req,res)=>{
+    let books;
     try{
        books = await  Book.find().sort({createdAt : 'desc'}).limit(10).exec();
     }
